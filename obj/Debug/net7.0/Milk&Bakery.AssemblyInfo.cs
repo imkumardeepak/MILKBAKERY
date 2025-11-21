28412485a3a7dc61f31452f9900b3d796b84edd4
imkumardeepak <deepakraznish@gmail.com>
2025-11-21 14:02:59 +0530

    feat: add PrintGatePass view for generating printable gate passes with vehicle and customer details, including a barcode.

--- obj/Debug/net7.0/Milk&Bakery.AssemblyInfo.cs
+++ obj/Debug/net7.0/Milk&Bakery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Milk&Bakery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1aa21bb1c7e99a9c2cadd1d705985a907d43bd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1714c263306ad065531e3fcfc0f0fbc0a33bde91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Milk&Bakery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Milk&Bakery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
